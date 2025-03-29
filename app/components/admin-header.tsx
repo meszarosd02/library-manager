f3bb2b84da73445f2ffdd666c7e7092f9f2ef22c
Mészáros Dominik <mdlolxd01@gmail.com>
2025-03-29 21:12:13 +0100

    admin header refactor

--- app/components/admin-header.tsx
+++ app/components/admin-header.tsx
@@ -2,18 +2,20 @@
 
 export default function AdminHeader(
     {
-        showDialog
+        showDialog,
+        title
     }
     : 
     {
-        showDialog: () => void
+        showDialog: () => void,
+        title: string
     }){
 
     return (
         <>
             <div className="w-screen h-12 bg-gray-400 p-2 flex flex-row items-center">
                 <div className="flex-1">
-                    <p className="md:text-2xl md:text-left max-md:text-center max-md:text-lg">Admin Books</p>
+                    <p className="md:text-2xl md:text-left max-md:text-center max-md:text-lg">{title}</p>
                 </div>
                 <div className="md:hidden cursor-pointer" onClick={showDialog}>
                     <p className="text-right p-2 rounded-lg bg-gray-700 hover:bg-gray-500">+</p>
