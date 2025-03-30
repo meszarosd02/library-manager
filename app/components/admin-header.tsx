4bf8ba2fb433a0a94e57018bdde0b27f12cd0a4c
Mészáros Dominik <mdlolxd01@gmail.com>
2025-03-30 19:16:32 +0200

    left side action button

--- app/components/admin-header.tsx
+++ app/components/admin-header.tsx
@@ -3,17 +3,22 @@
 export default function AdminHeader(
     {
         addAction,
+        backAction,
         title
     }
     : 
     {
         addAction?: () => void,
+        backAction?: () => void,
         title: string
     }){
 
     return (
         <>
             <div className="w-screen h-12 bg-gray-400 p-2 flex flex-row items-center">
+                <div className="md:hidden cursor-pointer" onClick={backAction}>
+                    <p className="text-left p-2 rounded-lg bg-gray-700 hover:bg-gray-500">-</p>
+                </div>
                 <div className="flex-1">
                     <p className="md:text-2xl md:text-left max-md:text-center max-md:text-lg">{title}</p>
                 </div>
