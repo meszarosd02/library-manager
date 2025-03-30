77c42e2e01e70e99ba1fd5ec40caf4fc521fd128
Mészáros Dominik <mdlolxd01@gmail.com>
2025-03-30 18:52:12 +0200

    clickable book entry in admin table

--- app/admin/books/books-table.tsx
+++ app/admin/books/books-table.tsx
@@ -1,6 +1,16 @@
 import { BookWithAuthors } from "@/app/lib/types";
 
-export default function BooksTable({books}: {books: BookWithAuthors[]}){
+export default function BooksTable(
+    {
+        books, 
+        onBookClicked 
+    }
+    :
+    { 
+        books: BookWithAuthors[],
+        onBookClicked: (book: BookWithAuthors) => void
+    }
+) {
     return (
         <>
             {books.length > 0 && <table className="border-collapse border border-gray-700">
@@ -13,7 +23,7 @@ export default function BooksTable({books}: {books: BookWithAuthors[]}){
                 </thead>
                 <tbody>
                     {books?.map((book) => (
-                        <tr key={book.id} className="[&>*]:p-2 [&>*]:border [&>*]:border-gray-700">
+                        <tr key={book.id} className="[&>*]:p-2 [&>*]:border [&>*]:border-gray-700" onClick={() => onBookClicked(book)}>
                             <td>{book.id}</td>
                             <td>{book.title}</td>
                             <td>{book.authors[0].name}</td>
