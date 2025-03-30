aa734d621a268775a0e4ccfe8fec4e65f825e381
Mészáros Dominik <mdlolxd01@gmail.com>
2025-03-30 19:17:19 +0200

    fix

--- app/components/admin-header.tsx
+++ app/components/admin-header.tsx
@@ -16,9 +16,9 @@ export default function AdminHeader(
     return (
         <>
             <div className="w-screen h-12 bg-gray-400 p-2 flex flex-row items-center">
-                <div className="md:hidden cursor-pointer" onClick={backAction}>
+                {backAction && <div className="md:hidden cursor-pointer" onClick={backAction}>
                     <p className="text-left p-2 rounded-lg bg-gray-700 hover:bg-gray-500">-</p>
-                </div>
+                </div>}
                 <div className="flex-1">
                     <p className="md:text-2xl md:text-left max-md:text-center max-md:text-lg">{title}</p>
                 </div>
