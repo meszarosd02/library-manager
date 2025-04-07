3ab2558c6caf2d5f7938f0420b8497e8a2410ada
Mészáros Dominik <mdlolxd01@gmail.com>
2025-04-07 13:06:56 +0200

    shelf section coordinates

--- app/components/shelf-view.tsx
+++ app/components/shelf-view.tsx
@@ -14,18 +14,11 @@ export default function ShelfView(
     }
 ) {
 
-    useEffect(() => {
-        const asd = async () => {
-            console.log(await getBookCountInShelf(1))
-        }
-        asd()
-    }, [])
-
     return (
         <>
             <div className={`grid`} style={{ gridTemplateColumns: `repeat(${shelf.col_num}, minmax(0, 1fr))`, gridTemplateRows: `repeat(${shelf.row_num}, minmax(0, 1fr))` }}>
-                {Array.from({ length: shelf.col_num * shelf.row_num }).map((_, i) => (
-                    <div key={i} className="border border-gray-200 p-2 h-10 cursor-pointer hover:bg-gray-600" onClick={() => onSectionClick(Math.floor(i / shelf.col_num), i % shelf.col_num)}></div>
+                {Array.from({ length: (shelf.col_num) * (shelf.row_num) }).map((_, i) => (
+                    <div key={i} className="border border-gray-200 p-2 h-10 cursor-pointer hover:bg-gray-600" onClick={() => onSectionClick(Math.floor(i / shelf.col_num), i % shelf.col_num)}>{Math.floor(i / shelf.col_num)+1},{(i % shelf.col_num) + 1}</div>
                 ))}
             </div>
         </>
